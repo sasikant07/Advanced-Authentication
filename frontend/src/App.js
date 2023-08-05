9f92b81dbc84cae4cf60cd05e04455809e2be80f
Sasikant Biswal <biswalsashikant@gmail.com>
2023-08-05 23:33:37 +0530

    Added some pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/home/Home";
+import Layout from "./components/layout/Layout";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Forgot from "./pages/auth/Forgot";
+import Reset from "./pages/auth/Reset";
+import LoginWithCode from "./pages/auth/LoginWithCode";
+import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Layout><Home /></Layout>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot" element={<Forgot />} />
+          <Route path="/resetPassword/:resetToken" element={<Reset />} />
+          <Route path="/loginWithCode/:email" element={<LoginWithCode />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/pages/auth/Register.js
@@ -0,0 +1,160 @@
+import React, {useEffect, useState} from 'react';
+import {TiUserAddOutline} from "react-icons/ti";
+import {FaTimes} from "react-icons/fa";
+import {BsCheck2All} from "react-icons/bs";
+import {Link} from "react-router-dom";
+import styles from "./auth.module.scss";
+import Card from '../../components/card/Card';
+import PasswordInput from '../../components/passwordInput/PasswordInput';
+
+const initialState = {
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+}
+
+const Register = () => {
+    const [formData, setFormData] = useState(initialState);
+    const {name, email, password, password2} = formData;
+
+    const [uCase, setUCase] = useState(false);
+    const [num, setNum] = useState(false);
+    const [sChar, setSChar] = useState(false);
+    const [passLength, setPassLength] = useState(false);
+
+    const timesIcon = <FaTimes color="red" size={15} />
+    const checkIcon = <BsCheck2All color="green" size={15} />
+
+    const switchIcon = (condition) => {
+        if (condition) {
+            return checkIcon;
+        }
+        return timesIcon;
+    }
+
+    
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({ ...formData, [name]: value });
+    }
+
+    useEffect(() => {
+        // Check Lower and Uppercase
+        if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
+            setUCase(true);
+        } else {
+            setUCase(false);
+        }
+
+        // Check for numbers
+        if (password.match(/([0-9])/)) {
+            setNum(true);
+        } else {
+            setNum(false);
+        }
+
+        // Check For Special char
+        if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) {
+            setSChar(true);
+        } else {
+            setSChar(false);
+        }
+
+        // Check Password Length
+        if (password.length > 5) {
+            setPassLength(true);
+        } else {
+            setPassLength(false);
+        }
+    }, [password])
+
+    const registerUser = (e) => {
+
+    }
+
+  return (
+    <div className={`container ${styles.auth}`}>
+        <Card>
+            <div className={styles.form}>
+                <div className="--flex-center">
+                    <TiUserAddOutline size={35} color="#999" />
+                </div>
+                <h2>Register</h2>
+
+                <form onSubmit={registerUser}>
+                    <input 
+                        type="text" 
+                        name="name" 
+                        placeholder="Name" 
+                        value={name} 
+                        onChange={handleInputChange} 
+                        required />
+                    <input 
+                        type="email" 
+                        name="email" 
+                        placeholder="Email" 
+                        value={email} 
+                        onChange={handleInputChange} 
+                        required />
+                    <PasswordInput 
+                        name="password" 
+                        placeholder="Password" 
+                        value={password} 
+                        onChange={handleInputChange}
+                    />
+                    <PasswordInput 
+                        name="password2" 
+                        placeholder="Confirm Password" 
+                        value={password2} 
+                        onChange={handleInputChange}
+                    />
+
+                    {/* Password Strength */}
+                    <Card cardClass={styles.group}>
+                        <ul className="form-list">
+                            <li>
+                                <span className={styles.indicator}>
+                                    {switchIcon(uCase)}
+                                        &nbsp;
+                                    Lowercase & Uppercase
+                                </span>
+                            </li>
+                            <li>
+                                <span className={styles.indicator}>
+                                    {switchIcon(num)}
+                                        &nbsp;
+                                    Number (0-9)
+                                </span>
+                            </li>
+                            <li>
+                                <span className={styles.indicator}>
+                                    {switchIcon(sChar)}
+                                        &nbsp;
+                                    Special Character (!@#$%^&*)
+                                </span>
+                            </li>
+                            <li>
+                                <span className={styles.indicator}>
+                                    {switchIcon(passLength)}
+                                        &nbsp;
+                                    At least 6 Character
+                                </span>
+                            </li>
+                        </ul>
+                    </Card>
+
+                    <button type="submit" className="--btn --btn-primary --btn-block">Register</button>
+                </form>
+                <span className={styles.register}>
+                    <Link to="/">Home</Link>
+                    <p>&nbsp; Already have an account? &nbsp;</p>
+                    <Link to="/login">Login</Link>
+                </span>
+            </div>
+        </Card>
+    </div>
+  )
+}
+
+export default Register;
