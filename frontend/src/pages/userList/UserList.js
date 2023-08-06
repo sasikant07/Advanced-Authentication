83ca96719f1c8616b2f7f5c140a669b8d332cbae
Sasikant Biswal <biswalsashikant@gmail.com>
2023-08-07 00:29:03 +0530

    Frontend all pages completed with structure

--- frontend/src/pages/userList/UserList.js
+++ frontend/src/pages/userList/UserList.js
@@ -1,28 +1,62 @@
-import React from 'react';
+import React from "react";
 import "./UserList.scss";
-import Pagemenu from '../../components/pageMenu/Pagemenu';
-import UserStats from '../../components/userStats/UserStats';
-import Search from '../../components/serach/Search';
+import Pagemenu from "../../components/pageMenu/Pagemenu";
+import UserStats from "../../components/userStats/UserStats";
+import Search from "../../components/serach/Search";
+import { FaTrashAlt } from "react-icons/fa";
+import ChangeRole from "../../components/changeRole/ChangeRole";
 
 const UserList = () => {
   return (
     <section>
-        <div className="container">
-            <Pagemenu />
-            <UserStats />
-            <div className="user-list">
-                <div className="--flex-between">
-                    <span>
-                        <h3>All Users</h3>
-                    </span>
-                    <span>
-                        <Search />
-                    </span>
-                </div>
+      <div className="container">
+        <Pagemenu />
+        <UserStats />
+
+        <div className="user-list">
+          <div className="table">
+            <div className="--flex-between">
+              <span>
+                <h3>All Users</h3>
+              </span>
+              <span>
+                <Search />
+              </span>
             </div>
+            {/* Table */}
+            <table>
+              <thead>
+                <tr>
+                  <th>s/n</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Role</th>
+                  <th>Change Role</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Zino</td>
+                  <td>Zino@gmail.com</td>
+                  <td>Admin</td>
+                  <td>
+                    <ChangeRole />
+                  </td>
+                  <td>
+                    <span>
+                      <FaTrashAlt size={20} color="red" />
+                    </span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default UserList;
