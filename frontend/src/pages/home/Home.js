4132bc20d8ee1cad2de7a77b07a8e59df053f87f
Sasikant Biswal <biswalsashikant@gmail.com>
2023-08-17 23:19:50 +0530

    redux setup

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./Home.scss";
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
@@ -14,8 +15,12 @@ const Home = () => {
           <p>Implement User Registration, Login, Password Reset, Social Login, User Permissions, Email Notifications etc.</p>
 
           <div className="hero-buttons --flex-start">
-            <button className="--btn --btn-danger">Register</button>
-            <button className="--btn --btn-primary">Login</button>
+            <button className="--btn --btn-danger">
+              <Link to="/register">Register</Link>
+            </button>
+            <button className="--btn --btn-primary">
+              <Link to="/login">Login</Link>
+            </button>
           </div>
         </div>
         <div className="hero-image">
